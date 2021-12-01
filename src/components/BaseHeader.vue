0cd8bf35a04f0229e252cc05c8646947f25ac25a
avelezdo <albertovelezdev@gmail.com>
2021-12-01 21:00:51 +0100

    Se crea el componente BaseHeader con el esqueleto básico

---
+++ src/components/BaseHeader.vue
@@ -0,0 +1,12 @@
+<template lang="">
+    <header>
+        <img src="" alt="" />
+        <div>
+            <iconSearch />
+        </div>
+    </header>
+</template>
+<script>
+export default {}
+</script>
+<style lang=""></style>
