a0b2be9fc66f282a8d0b77fdfe3c6f85fc2beee5
avelezdo <albertovelezdev@gmail.com>
2021-12-08 20:21:23 +0100

    Se terminan los estilos del VideoItem

--- src/components/VideoItem.vue
+++ src/components/VideoItem.vue
@@ -1,16 +1,29 @@
 <template>
-    <img :src="video.thumbnail" :alt="video.title" />
-    <span>{{ video.duration }}</span>
     <div>
+        <div class="relative">
+            <img :src="video.thumbnail" :alt="video.title" />
+            <span
+                class="absolute bottom-0 right-0 bg-black m-2 p-0.5 text-white font-bold text-sm"
+                >{{ video.duration }}</span
+            >
+        </div>
+    </div>
+
+    <div class="p-4 flex items-start">
         <img
             :src="video.channelImg"
             :alt="`youtube channel ${video.channelName}`"
+            class="rounded-full w-10 h-10 mr-4"
         />
+        <div>
+            <h4 class="text-sm font-bold video-title">{{ video.title }}</h4>
+            <p class="text-xs">
+                {{
+                    `${video.channelName} ${video.visualizations} de ${video.date}`
+                }}
+            </p>
+        </div>
     </div>
-    <h4>{{ video.title }}</h4>
-    <p>
-        {{ `${video.channelName} ${video.visualizations} de ${video.date}` }}
-    </p>
 </template>
 
 <script setup>
@@ -21,3 +34,15 @@ defineProps({
     },
 })
 </script>
+
+<style scoped>
+.video-title {
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
+    max-height: 2.5rem;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+}
+</style>
