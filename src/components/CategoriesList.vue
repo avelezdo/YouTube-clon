9407611a553d9e269d0c1e83aa4847cad9638bef
avelezdo <albertovelezdev@gmail.com>
2021-12-16 17:53:42 +0100

    Añadida funcionalidad básica de los filtros por categorías (chips)

--- src/components/CategoriesList.vue
+++ src/components/CategoriesList.vue
@@ -1,16 +1,31 @@
 <template>
-    <section><Chip text="Todo" is-selected="true"></Chip></section>
-    <section>
+    <section class="p-2 flex overflow-scroll">
+        <Chip text="Todo" :is-selected="isAllSelected"></Chip>
         <Chip
             v-for="category in categories"
             :key="category.id"
             :text="category.name"
+            :is-selected="checkIfIsSelected(category.id)"
+            @click="selectCategory(category.id)"
         ></Chip>
     </section>
 </template>
 
 <script setup>
 import Chip from './Chip.vue'
+import { computed, ref } from '@vue/reactivity'
+
+const selectedCategory = ref()
+const isAllSelected = computed(() => {
+    return !selectedCategory.value
+})
+const checkIfIsSelected = categoryId => {
+    return selectedCategory.value === categoryId
+}
+const selectCategory = categoryId => {
+    selectedCategory.value = categoryId
+}
+
 defineProps({
     categories: {
         type: Array,
--- src/components/Chip.vue
+++ src/components/Chip.vue
@@ -1,10 +1,19 @@
 <template>
-    <span class="chip" :class="chipClasess">{{ text }}</span>
-    <!-- <span class="chip" :class="isSelected ? 'selected' : ''">{{ text }}</span> -->
+    <div class="my-2 mr-2 text-sm" @click="emitClickEvent">
+        <span class="chip border p-2 rounded-full" :class="chipClasess">{{
+            text
+        }}</span>
+    </div>
 </template>
 
 <script setup>
 import { computed } from '@vue/reactivity'
+
+const emits = defineEmits(['click'])
+const emitClickEvent = () => {
+    emits('click')
+}
+
 const props = defineProps({
     text: {
         type: String,
@@ -20,3 +29,14 @@ const chipClasess = computed(() => {
     return { selected: props.isSelected }
 })
 </script>
+
+<style scoped>
+.chip {
+    border-color: rgba(0, 0, 0, 0.1);
+    background-color: rgb(0 0 0 / 5%);
+}
+.chip.selected {
+    background-color: #606060;
+    color: white;
+}
+</style>
