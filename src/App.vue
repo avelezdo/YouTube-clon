795ae484c444762c35c5c3bb5a2507c178ab48d8
avelezdo <albertovelezdev@gmail.com>
2021-12-21 20:27:11 +0100

    Se termina de implementar el selector de categorías

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
 <script setup>
+import { ref } from '@vue/reactivity'
 import BaseHeader from './components/BaseHeader.vue'
 import VideosList from './components/VideosList.vue'
 import CategoriesList from './components/CategoriesList.vue'
 import { videos } from './assets/videos.json'
+
+let filteredVideos = ref(videos)
 import { categories } from './assets/categories.json'
+
+const selectVideosWithCategory = categoryId => {
+    filteredVideos.value =
+        categoryId === 0
+            ? videos
+            : videos.filter(v => v.category === categoryId)
+}
 </script>
 
 <template>
     <BaseHeader></BaseHeader>
-    <CategoriesList :categories="categories"></CategoriesList>
-    <VideosList :videos="videos" />
+    <CategoriesList
+        :categories="categories"
+        @select-category="selectVideosWithCategory"
+    ></CategoriesList>
+    <VideosList :videos="filteredVideos" />
 </template>
